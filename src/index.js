073b79a8018ca3c8d6509fcf32dc441ea3ca3ab4
Marco Santana <santannacostamarco@gmail.com>
2020-04-24 08:50:06 -0300

    connected to api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { 
+  Text, 
+  StatusBar, 
+  StyleSheet, 
+  FlatList,
+  TouchableOpacity
+} from 'react-native';
+import api from './services/api';
+
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: "#007462"
+  }
+})
 
 export default function App () {
+
+  const [projects, setProjects] = useState([])
+  const handlePress = async () => {
+    const response = await api.post('projects', {
+      title: `Project date ${Date.now()}`,
+      owner: `Project name ${Date.now()}`
+    })
+
+    setProjects([...projects, response.data])
+  }
+
+  useEffect(()=> {
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
   return (
-    <View>
-      <Text>
-        Hello, World!
-      </Text>
-    </View>
+    <>
+      <StatusBar backgroundColor="#008000" barStyle="light-content"/>
+      <FlatList style={styles.app}
+        data={projects}
+        keyExtractor={project => project.id}
+        renderItem={({ item: project}) => (
+          <Text key={project.id}> { project.title } </Text>
+        )} />
+        <TouchableOpacity
+          onPress={ () => handlePress() } >
+          <Text>
+            New Project
+          </Text>
+        </TouchableOpacity>
+    </>
   )
 }
